b1965f334e03a846f7b659b0cd5ffda482a870d1
LoftyLettuce <lubolulubo2@gmail.com>
2025-07-23 11:24:40 +0700

    fix: change button name, optimize code

--- background.js
+++ background.js
@@ -1,17 +1,6 @@
-async function checkItem(taskId, itemId){
-  const response = await fetch(`https://habitica.com/api/v3/tasks/${taskId}/checklist/${itemId}/score`, {
-    method: 'POST', 
-    mode: 'cors',
-    headers: {
-        'x-api-user' : '7a9a0dee-b79b-4596-8a77-99ca5efe983c', 
-        'x-api-key' : '9b26b414-1935-4d08-af35-827cfe93bf89',
-        "x-client": '7a9a0dee-b79b-4596-8a77-99ca5efe983c-Pomodo',
-      },
-    })
-}
-async function checkTask(taskId){
-  const response = await fetch(`https://habitica.com/api/v3/tasks/${taskId}/score/up`, {
-    method: 'POST', 
+async function fetchItems(id){
+  const response = await fetch(`https://habitica.com/api/v3/tasks/${id}`, {
+    method: 'GET', 
     mode: 'cors',
     headers: {
         'x-api-user' : '7a9a0dee-b79b-4596-8a77-99ca5efe983c', 
@@ -20,14 +9,68 @@ async function checkTask(taskId){
       },
     })
   const task = await response.json();
-  console.log(task);
+  return task.data.checklist.filter((val)=>!val.completed).map((val)=>val.id);
+}
+async function connectTask(){
+  chrome.storage.local.get('Task', (result)=>{
+    if (!('Task' in result)){
+      fetch(`https://habitica.com/api/v3/tasks/user`, {
+        method: 'GET', 
+        mode: 'cors',
+        headers: {
+            'x-api-user' : '7a9a0dee-b79b-4596-8a77-99ca5efe983c', 
+            'x-api-key' : '9b26b414-1935-4d08-af35-827cfe93bf89',
+            "x-client": '7a9a0dee-b79b-4596-8a77-99ca5efe983c-Pomodo',
+          },
+        })
+      .then(async (Response)=>{
+          const tasks = await Response.json();
+          const pomodoData = tasks.data.filter((task)=>(task.text=='Overflow'||task.text=='Study'));
+          let infoObject = {};
+          pomodoData.forEach((val) => {
+            infoObject[val.type] = val.id;
+          });
+          chrome.storage.local.set({'Task': JSON.stringify(infoObject)});
+        })
+      .catch((error)=>console.log(error));
+    }
+  })
+}
+async function connectList(){
+  chrome.storage.local.get('Task', async (result)=>{
+    const task = JSON.parse(result.Task);
+    let checkList = await fetchItems(task.daily);
+    chrome.storage.local.set({'CheckList': JSON.stringify(checkList)})
+  })
+}
+function checkItem(taskId, itemId){
+  fetch(`https://habitica.com/api/v3/tasks/${taskId}/checklist/${itemId}/score`, {
+    method: 'POST', 
+    mode: 'cors',
+    headers: {
+        'x-api-user' : '7a9a0dee-b79b-4596-8a77-99ca5efe983c', 
+        'x-api-key' : '9b26b414-1935-4d08-af35-827cfe93bf89',
+        "x-client": '7a9a0dee-b79b-4596-8a77-99ca5efe983c-Pomodo',
+      },
+    })
+}
+function checkTask(taskId){
+  fetch(`https://habitica.com/api/v3/tasks/${taskId}/score/up`, {
+    method: 'POST', 
+    mode: 'cors',
+    headers: {
+        'x-api-user' : '7a9a0dee-b79b-4596-8a77-99ca5efe983c', 
+        'x-api-key' : '9b26b414-1935-4d08-af35-827cfe93bf89',
+        "x-client": '7a9a0dee-b79b-4596-8a77-99ca5efe983c-Pomodo',
+      },
+    })
 }
 chrome.runtime.onMessage.addListener((message)=>{
   chrome.alarms.create("countdownTimer", {
     delayInMinutes: message.time,
   })
 })
-chrome.alarms.onAlarm.addListener(()=>{
+chrome.alarms.onAlarm.addListener(async ()=>{
   chrome.notifications.create({
     type: "basic",
     iconUrl: "48-icon.png",
@@ -36,6 +79,8 @@ chrome.alarms.onAlarm.addListener(()=>{
     priority: 2,
   });
   chrome.storage.local.remove('Time');
+  await connectTask();
+  await connectList();
   chrome.storage.local.get(['CheckList', 'Task'], async (result)=>{
     if ('CheckList' in result && 'Task' in result){
       const tasks = JSON.parse(result.Task);
--- main.js
+++ main.js
@@ -1,19 +1,4 @@
 /// <reference types="chrome" />
-// fetch(`https://habitica.com/api/v3/tasks/user`, {
-  //   method: 'GET', 
-  //   mode: 'cors',
-  //   headers: {
-    //     'x-api-user' : '7a9a0dee-b79b-4596-8a77-99ca5efe983c', 
-    //     'x-api-key' : '9b26b414-1935-4d08-af35-827cfe93bf89',
-    //     "x-client": '7a9a0dee-b79b-4596-8a77-99ca5efe983c-Pomodo',
-    //   },
-    // })
-    // .then(async (Response)=>{
-      //   const tasks = await Response.json();
-      //   const pomodoData = tasks.data.filter((task)=>(task.text=='Overflow'||task.text=='Study'));
-      //   console.log(pomodoData);
-      // })
-      // .catch((error)=>console.log(error));
 function timer(goal, s){
   setTimeout(()=>{
     let countDownUI = document.querySelector('div');
@@ -29,52 +14,7 @@ function timer(goal, s){
     }
   }, 1000);
 }
-async function fetchItems(id){
-  const response = await fetch(`https://habitica.com/api/v3/tasks/${id}`, {
-    method: 'GET', 
-    mode: 'cors',
-    headers: {
-        'x-api-user' : '7a9a0dee-b79b-4596-8a77-99ca5efe983c', 
-        'x-api-key' : '9b26b414-1935-4d08-af35-827cfe93bf89',
-        "x-client": '7a9a0dee-b79b-4596-8a77-99ca5efe983c-Pomodo',
-      },
-    })
-  const task = await response.json();
-  return task.data.checklist.filter((val)=>!val.completed).map((val)=>val.id);
-}
-async function connectTask(){
-  chrome.storage.local.get('Task', (result)=>{
-    if (!('Task' in result)){
-      fetch(`https://habitica.com/api/v3/tasks/user`, {
-        method: 'GET', 
-        mode: 'cors',
-        headers: {
-            'x-api-user' : '7a9a0dee-b79b-4596-8a77-99ca5efe983c', 
-            'x-api-key' : '9b26b414-1935-4d08-af35-827cfe93bf89',
-            "x-client": '7a9a0dee-b79b-4596-8a77-99ca5efe983c-Pomodo',
-          },
-        })
-      .then(async (Response)=>{
-          const tasks = await Response.json();
-          const pomodoData = tasks.data.filter((task)=>(task.text=='Overflow'||task.text=='Study'));
-          let infoObject = {};
-          pomodoData.forEach((val) => {
-            infoObject[val.type] = val.id;
-          });
-          chrome.storage.local.set({'Task': JSON.stringify(infoObject)});
-        })
-      .catch((error)=>console.log(error));
-    }
-  })
-}
-function connectList(){
-  chrome.storage.local.get('Task', async (result)=>{
-    const task = JSON.parse(result.Task);
-    let checkList = await fetchItems(task.daily);
-    chrome.storage.local.set({'CheckList': JSON.stringify(checkList)})
-  })
-}
-async function checkTimer(){
+async function checkTimer(session){
   const result = await chrome.storage.local.get('Time');
   if ('Time' in result){
     const savedTime = dateFns.parseJSON(result.Time);
@@ -83,20 +23,20 @@ async function checkTimer(){
 
   }
   else if (!('Time' in result)){
-    return {timeLeft:60};
+    return {timeLeft:session};
   }
 }
-async function showTime(){
+async function showTime(session){
   const btn = document.querySelector('button');
   btn.disabled = true;
   document.querySelector('button').disabled = true;
-  const result = await checkTimer();
+  const result = await checkTimer(session);
   const clock = {
     minute: `${Math.floor(result.timeLeft/60)}`.padStart(2, '0'),
     second: `${result.timeLeft%60}`.padStart(2, '0')
   };
   document.querySelector('div').textContent = `${clock.minute} : ${clock.second}`;
-  if (result.timeLeft < 60 && result.timeLeft > 0){
+  if (result.timeLeft < session && result.timeLeft > 0){
     timer(result.goal, result.timeLeft);
   }
   else{
@@ -104,9 +44,8 @@ async function showTime(){
   }
 }
 window.addEventListener('load', async ()=>{
-  connectTask().then(()=>{connectList()});
-  showTime();
-  const session = 1;
+  const session = 20;
+  showTime(session*60);
   document.querySelector('button').addEventListener('click', (e)=>{
     e.target.disabled = true;
     const goal = dateFns.addMinutes(new Date(), session)
